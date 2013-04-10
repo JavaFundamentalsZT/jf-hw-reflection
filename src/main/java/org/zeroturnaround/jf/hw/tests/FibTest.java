901e2f2778a77bb2d4c69610dae4b12701979b46
Anton Arhipov <anton@zeroturnaround.com>
2013-04-10 18:21:29 +0300

    some hints for students

--- src/main/java/org/zeroturnaround/jf/hw/tests/FibTest.java
+++ src/main/java/org/zeroturnaround/jf/hw/tests/FibTest.java
@@ -7,6 +7,22 @@ import org.zeroturnaround.jf.hw.framework.Test;
 
 import static org.zeroturnaround.jf.hw.framework.Assert.assertEquals;
 
+//    Expected test execution output:
+//
+//    ------------------------------------------------------------------------------
+//    SUCCESS: FibTest.find1stFibonacciNumber
+//    SUCCESS: FibTest.find2ndAnd3rdFibonacciNumber
+//    SUCCESS: FibTest.find10thFibonacciNumber
+//    FAILURE: FibTest.find15thFibonacciNumberWithFailure: EXPECTED: 55, ACTUAL: 610
+//    SUMMARY: EXECUTED 4, PASSED 3, FAILED 1, BUSINESS METHODS INVOKED 5
+//    ------------------------------------------------------------------------------
+//
+//    Notes:
+//    EXECUTED 4 - FibTest has 4 methods marked with @Test annotation and 4 of them were executed
+//    PASSED 3 - 3 tests passed successfully
+//    FAILED 1 - 1 test failed (find15thFibonacciNumberWithFailure)
+//    BUSINESS METHODS INVOKED 5 - fib.calculate() was invoked 5 times
+
 @Test
 public class FibTest {
 
